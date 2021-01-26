75ea10fe5de0bf605c58e471ca53f414e52e31c3
cristinanagy <58230925+cristinanagy@users.noreply.github.com>
2021-01-26 19:51:49 +0200

    first commit

---
+++ app/Http/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Task;
+use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/', [RegistrationController::class, 'create']);
+
+Route::get('/', 'TaskController@index'); 
+Route::resource('tasks', 'TaskController');I 
